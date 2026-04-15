f0edf4d476ee7c05aab7d9cfc88834e0036d5626
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2026-04-15 13:37:20 +0200

    bugfix: bruk riktig adresse mot PDL (#44)
    * bugfix: bruk riktig adresse mot PDL
    
    * feil i default

--- src/main/java/no/nav/foreldrepenger/los/persontjeneste/PdlKlient.java
+++ src/main/java/no/nav/foreldrepenger/los/persontjeneste/PdlKlient.java
@@ -4,7 +4,9 @@ import no.nav.vedtak.felles.integrasjon.person.AbstractPersonKlient;
 import no.nav.vedtak.felles.integrasjon.rest.RestClientConfig;
 import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 
-@RestClientConfig(tokenConfig = TokenFlow.ADAPTIVE, endpointProperty = "pdl.base.url", endpointDefault = "http://pdl-api.pdl/graphql", scopesProperty = "pdl.scopes", scopesDefault = "api://prod-fss.pdl.pdl-api/.default")
+@RestClientConfig(tokenConfig = TokenFlow.ADAPTIVE, endpointProperty = "pdl.base.url",
+    endpointDefault = "https://pdl-api.prod-fss-pub.nais.io/graphql",
+    scopesProperty = "pdl.scopes", scopesDefault = "api://prod-fss.pdl.pdl-api/.default")
 public class PdlKlient extends AbstractPersonKlient {
 
     public PdlKlient() {
