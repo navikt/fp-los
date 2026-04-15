9a244b86b53aba0de4ab5ed767fbce74e4401f1c
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2026-04-15 13:36:24 +0200

    feil i default

--- src/main/java/no/nav/foreldrepenger/los/persontjeneste/PdlKlient.java
+++ src/main/java/no/nav/foreldrepenger/los/persontjeneste/PdlKlient.java
@@ -5,7 +5,7 @@ import no.nav.vedtak.felles.integrasjon.rest.RestClientConfig;
 import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 
 @RestClientConfig(tokenConfig = TokenFlow.ADAPTIVE, endpointProperty = "pdl.base.url",
-    endpointDefault = "pdl.base.url=https://pdl-api.prod-fss-pub.nais.io/graphql",
+    endpointDefault = "https://pdl-api.prod-fss-pub.nais.io/graphql",
     scopesProperty = "pdl.scopes", scopesDefault = "api://prod-fss.pdl.pdl-api/.default")
 public class PdlKlient extends AbstractPersonKlient {
 
