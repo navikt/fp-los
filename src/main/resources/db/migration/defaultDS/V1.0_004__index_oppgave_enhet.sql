5766bc24e4220b3e7edce7e77667f18e8084d98c
Anders Palfi <anders.palfi@nav.no>
2026-04-20 12:09:12 +0200

    Ikke concurrently

--- src/main/resources/db/migration/defaultDS/V1.0_004__index_oppgave_enhet.sql
+++ src/main/resources/db/migration/defaultDS/V1.0_004__index_oppgave_enhet.sql
@@ -1,6 +1,4 @@
--- flyway:executeInTransaction=false
-
 -- Indeks for oppgavekø-spørringer som filtrerer på behandlende_enhet uten aktiv-filter.
 -- Eksisterende idx_oppgave_enhet_aktiv(aktiv, behandlende_enhet) har feil kolonnerekkefølge
 -- for disse spørringene.
-CREATE INDEX CONCURRENTLY idx_oppgave_enhet ON oppgave (behandlende_enhet);
+CREATE INDEX idx_oppgave_enhet ON oppgave (behandlende_enhet);
