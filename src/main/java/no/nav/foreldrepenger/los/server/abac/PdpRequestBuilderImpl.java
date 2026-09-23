a85f83abae0ce8c6b10b42f81556d57338a7d4b7
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2026-09-23 12:55:41 +0200

    TFP-7063 samle logging og bruk standardiserte logg-felt-navn

--- src/main/java/no/nav/foreldrepenger/los/server/abac/PdpRequestBuilderImpl.java
+++ src/main/java/no/nav/foreldrepenger/los/server/abac/PdpRequestBuilderImpl.java
@@ -6,10 +6,8 @@ import jakarta.annotation.Priority;
 import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Alternative;
 import jakarta.inject.Inject;
-import no.nav.foreldrepenger.los.oppgave.Oppgave;
 import no.nav.foreldrepenger.los.oppgave.OppgaveTjeneste;
 import no.nav.foreldrepenger.los.tjenester.avdelingsleder.saksliste.FplosAbacAttributtType;
-import no.nav.vedtak.log.mdc.MdcExtendedLogContext;
 import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
 import no.nav.vedtak.sikkerhet.abac.PdpRequestBuilder;
 import no.nav.vedtak.sikkerhet.abac.pdp.AppRessursData;
@@ -21,8 +19,6 @@ import no.nav.vedtak.sikkerhet.abac.pipdata.PipFagsakStatus;
 @Priority(2)
 public class PdpRequestBuilderImpl implements PdpRequestBuilder {
 
-    private static final MdcExtendedLogContext MDC_EXTENDED_LOG_CONTEXT = MdcExtendedLogContext.getContext("prosess");
-
     private final OppgaveTjeneste oppgaveTjeneste;
 
     @Inject
@@ -33,39 +29,16 @@ public class PdpRequestBuilderImpl implements PdpRequestBuilder {
     @Override
     public AppRessursData lagAppRessursData(AbacDataAttributter dataAttributter) {
 
-        var oppgave = hentOppgave(dataAttributter);
-        if (oppgave == null) {
-            return minimalbuilder().build();
-        }
-
-        setLogContext(oppgave);
-        return minimalbuilder()
-            .medSaksnummer(oppgave.getSaksnummer().getVerdi())
-            .build();
-    }
-
-    @Override
-    public AppRessursData lagAppRessursDataForSystembruker(AbacDataAttributter dataAttributter) {
-        setLogContext(hentOppgave(dataAttributter));
-        return minimalbuilder().build();
-    }
-
-    private static void setLogContext(Oppgave oppgave) {
-        if (oppgave == null) {
-            return;
-        }
-        MDC_EXTENDED_LOG_CONTEXT.add("fagsak", oppgave.getSaksnummer().getVerdi());
-        MDC_EXTENDED_LOG_CONTEXT.add("behandling", oppgave.getBehandling().getId());
-    }
-
-    private Oppgave hentOppgave(AbacDataAttributter dataAttributter) {
         Set<Long> oppgaveIdList = dataAttributter.getVerdier(FplosAbacAttributtType.OPPGAVE_ID);
-        return oppgaveIdList.stream().findFirst().map(oppgaveTjeneste::hentOppgave).orElse(null);
-    }
+        var oppgave = oppgaveIdList.stream().findFirst().map(oppgaveTjeneste::hentOppgave);
 
-    private AppRessursData.Builder minimalbuilder() {
-        return AppRessursData.builder()
+        var builder = AppRessursData.builder()
             .medFagsakStatus(PipFagsakStatus.UNDER_BEHANDLING)
             .medBehandlingStatus(PipBehandlingStatus.UTREDES);
+        oppgave.ifPresent(o -> builder
+            .medSaksnummer(o.getSaksnummer().getVerdi())
+            .medLoggSaksnummer(o.getSaksnummer().getVerdi())
+            .medLoggBehandling(o.getBehandling().getId()));
+        return builder.build();
     }
 }
